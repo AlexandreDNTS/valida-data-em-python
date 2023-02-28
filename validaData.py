79754efc8767dc439763f5f4a205d929a2935336
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2023-02-28 20:53:26 -0300

    atualizando as condições de validação

--- validaData.py
+++ validaData.py
@@ -1,7 +1,7 @@
 def DataValida(data):
     dia,mes,ano = map(int,data.split('/'))
 
-    if mes <=0 or mes > 31 and ano <=0:
+    if mes <=0 or mes > 12 and ano <=0:
         return False
     if mes in (1,3,5,7,8,10,12):
         UltimoDia = 31
